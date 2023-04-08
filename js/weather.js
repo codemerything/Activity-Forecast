144f73e022bf244da2917f62b61f6520e49f6d50
mmnldm <mremmanueledem@gmail.com>
2023-04-08 16:45:29 +0100

    get weather data from api and display object

---
+++ js/weather.js
@@ -0,0 +1,45 @@
+export class Weather{
+    constructor(apiKey) {
+        this.apiKey = apiKey,
+        this.apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
+        this.searchBox = document.querySelector('.search'),
+        this.city = document.querySelector('.city'),
+        this.celc = document.querySelector('.celc'),
+        this.weatherIcon = document.getElementById('weather-icon'),
+        this.wind = document.querySelector('.wind'),
+        this.humiditiy = document.querySelector('.humidity'),
+        this.weathercondition = document.querySelector('.weatcond'),
+
+       this.setupListeners()
+    }
+
+    setupListeners(){
+        this.searchBox.addEventListener('keydown',async(event) => {
+            if (event.key === 'Enter'){
+                event.preventDefault();
+                this.checkWeather(this.searchBox.value);
+                this.searchBox = '';
+            }
+        })
+    }
+
+    async checkWeather(city){
+        const response = await fetch(`${this.apiUrl}${city}`);
+        let data = await response.json();
+
+        this.city.innerHTML = data.location.name;
+        this.celc.textContent = Math.floor(data.current.temp_c);
+        this.humiditiy.textContent = data.current.humidity + '%';
+        this.wind.textContent = data.current.wind_mph + 'mph';
+        this.weatherIcon.src = data.current.condition.icon;
+        this.weathercondition.innerHTML = data.current.condition.text;
+
+        this.pickActivity();
+    }
+
+
+
+}
+
+const apiKey = '42f48ac9922e41b199293959230804'
+const weatherApp = new Weather(apiKey);
